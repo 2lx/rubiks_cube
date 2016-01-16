8c69e6b88cf561e59f335455b751132a07f2bd20
Alex <lexaaim@gmail.com>
2016-01-16 09:29:00 +0300

    fix: fix locking fps

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,15 @@ int main( int argc, char * args[] )
 {
 	CGameEngine game;
 
-	// initialize the engine
 	game.Init( "Engine Test v1.0" );
-
-	// load the intro
 	game.ChangeState( CPlayState::Instance() );
-
-	Uint32 start;
-	SDL_Event event;
 
 	while ( game.Running() )
 	{
-		start = SDL_GetTicks();
-
 		game.HandleEvents();
 		game.Update();
 		game.Draw();
 
-		if ( SDL_GetTicks() - start < SCREEN_TICK_PER_FRAME )
-			SDL_Delay( SCREEN_TICK_PER_FRAME - ( SDL_GetTicks() - start ) );
 	}
 
 	game.Cleanup();
